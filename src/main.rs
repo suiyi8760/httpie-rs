71348a9e90419ccd149c98ebaf0aafe3497e0c5e
Johnny <suiyi8760@sina.com>
2022-12-13 00:21:36 +0800

    feat: clap初体验

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use clap::{Parser, Subcommand};
+use reqwest::Url;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -7,10 +9,40 @@ struct Opts {
     subcmd: Subcmd,
 }
 
-#[derive(Subcommand,Debug)]
-enum Subcmd {}
+#[derive(Subcommand, Debug)]
+enum Subcmd {
+    Get(Get),
+    Post(Post),
+}
+
+#[derive(Debug, Parser)]
+struct Get {
+    #[arg(value_parser = parse_url)]
+    url: String,
+}
+
+#[derive(Debug, Parser)]
+struct Post {
+    #[arg(value_parser = parse_url)]
+    url: String,
+    body: Vec<String>,
+}
+
+// 校验url参数是否符合url规则
+fn parse_url(s: &str) -> Result<String> {
+    // 通过能否使用str.parse()强制转换成功转换成reqwest的Url类型，判断url字符串是否符合url规范
+    let _url: Url = s.parse()?;
+    Ok(s.into())
+}
+
+/// 命令行中的 key=value 可以通过 parse_kv_pair 解析成 KvPair 结构
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
 
 fn main() {
-    let opts = Opts::parse(); 
+    let opts = Opts::parse();
     println!("{:?}", opts);
 }
